d4cf02e9b8f7039ac35d7e6833b767a52c031930
Luca Neumann <81520529+luca-neumann@users.noreply.github.com>
2024-08-17 15:17:24 +0200

    Update DiplomarbeitClient.ino

--- DiplomarbeitClient/DiplomarbeitClient.ino
+++ DiplomarbeitClient/DiplomarbeitClient.ino
@@ -1,113 +1,50 @@
-/*
+// Define the RX and TX pins for hardware serial
+const int rxPin = 16;
+const int txPin = 17;
 
+// Create a HardwareSerial object (Serial1 in case of ESP32)
+HardwareSerial pmsSerial(2);
 
-  Webseite: https://dronebotworkshop.com/air-quality/
+unsigned char data[32];  // Data buffer for reading sensor data
 
-
-  PM2.5 Demo
-  pm25-demo.ino
-  Demonstrates operation of PM2.5 Particulate Matter Sensor
-  ESP32 Serial Port (RX = 16, TX = 17)
-  Derived from howtoelectronics.com - https://how2electronics.com/interfacing-pms5003-air-quality-sensor-arduino/
- 
-  DroneBot Workshop 2022
-  https://dronebotworkshop.com
-*/
- 
-// Serial Port connections for PM2.5 Sensor
-#define RXD2 16 // To sensor TXD
-#define TXD2 17 // To sensor RXD
- 
 void setup() {
-  // our debugging output
-  Serial.begin(115200);
- 
-  // Set up UART connection
-  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  // Start the hardware serial communication for debug
+  Serial.begin(9600);
+  // Start the hardware serial communication for the PMS7003
+  pmsSerial.begin(9600, SERIAL_8N1, rxPin, txPin);
 }
- 
-struct pms5003data {
-  uint16_t framelen;
-  uint16_t pm10_standard, pm25_standard, pm100_standard;
-  uint16_t pm10_env, pm25_env, pm100_env;
-  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
-  uint16_t unused;
-  uint16_t checksum;
-};
- 
-struct pms5003data data;
- 
+
 void loop() {
-  if (readPMSdata(&Serial1)) {
-    // reading data was successful!
-    Serial.println();
-    Serial.println("---------------------------------------");
-    Serial.println("Concentration Units (standard)");
-    Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
-    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
-    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
-    Serial.println("---------------------------------------");
-    Serial.println("Concentration Units (environmental)");
-    Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
-    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_env);
-    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_env);
-    Serial.println("---------------------------------------");
-    Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
-    Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
-    Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
-    Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
-    Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
-    Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
-    Serial.println("---------------------------------------");
-  }
-}
- 
-boolean readPMSdata(Stream *s) {
-  if (! s->available()) {
-    return false;
-  }
- 
-  // Read a byte at a time until we get to the special '0x42' start-byte
-  if (s->peek() != 0x42) {
-    s->read();
-    return false;
-  }
- 
-  // Now read all 32 bytes
-  if (s->available() < 32) {
-    return false;
-  }
- 
-  uint8_t buffer[32];
-  uint16_t sum = 0;
-  s->readBytes(buffer, 32);
- 
-  // get checksum ready
-  for (uint8_t i = 0; i < 30; i++) {
-    sum += buffer[i];
-  }
- 
-  /* debugging
-    for (uint8_t i=2; i<32; i++) {
-    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+  Serial.println("error");
+  if (pmsSerial.available()) {
+    Serial.println("Data available from sensor.");
+    // Wait until there is enough data (32 bytes) to read
+    if (pmsSerial.read() == 0x42) {  // Header byte 1
+      if (pmsSerial.read() == 0x4d) {  // Header byte 2
+        for (int i = 0; i < 30; i++) {
+          data[i] = pmsSerial.read();
+        }
+        
+        // Data parsing
+        unsigned int PM1_0 = ((data[10] << 8) + data[11]);  // PM1.0 concentration (ug/m3)
+        unsigned int PM2_5 = ((data[12] << 8) + data[13]);  // PM2.5 concentration (ug/m3)
+        unsigned int PM10  = ((data[14] << 8) + data[15]);  // PM10 concentration (ug/m3)
+        
+        // Print the PM values to the serial monitor
+        Serial.print("PM1.0: ");
+        Serial.print(PM1_0);
+        Serial.print(" ug/m3, ");
+        
+        Serial.print("PM2.5: ");
+        Serial.print(PM2_5);
+        Serial.print(" ug/m3, ");
+        
+        Serial.print("PM10: ");
+        Serial.print(PM10);
+        Serial.println(" ug/m3");
+        
+        delay(1000);  // Wait a second before reading again
+      }
     }
-    Serial.println();
-  */
- 
-  // The data comes in endian'd, this solves it so it works on all platforms
-  uint16_t buffer_u16[15];
-  for (uint8_t i = 0; i < 15; i++) {
-    buffer_u16[i] = buffer[2 + i * 2 + 1];
-    buffer_u16[i] += (buffer[2 + i * 2] << 8);
   }
- 
-  // put it into a nice struct :)
-  memcpy((void *)&data, (void *)buffer_u16, 30);
- 
-  if (sum != data.checksum) {
-    Serial.println("Checksum failure");
-    return false;
-  }
-  // success!
-  return true;
 }
